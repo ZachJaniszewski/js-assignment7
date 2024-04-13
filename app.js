95911bbae2857140457e88b6f8721dd7f3548a96
Zachary Janiszewski <300088297@fvtc.edu>
2024-04-13 11:17:21 -0500

    First commit

--- app.js
+++ app.js
@@ -17,14 +17,29 @@ app.get('/', (_, response) => {
 	response.sendFile('index.html', { root })
 })
 
-
-
 // GET /api/todos
+app.get('/api/todos', (request, response) => {
+	response.json(todos);
+})
 
 // POST /api/todos
+app.post('/api/todos', (request, response) => {
+	const { item } = request.body
+	const id = todos.length + 1
+	const complete = false
+	todos.push({ id, item, complete })
+	response.json({ id });
+
+})
 
 // PUT /api/todos/:id
+app.put('/api/todos/:id', (request, response) => {
+	const { id } = request.params
+	const task = todos.find(todo => todo.id.toString() === id)
+	task.complete = !task.complete;
+	response.json({id: task.id, complete: task.complete});
 
+})
 
 
 const message = `Server running: http://localhost:${port}`
